0d973e4e885b3ec9ac62dd384d088aeac07ec9da
pcarbonero <pcarbonero@106-19.info2>
2023-09-26 10:15:38 +0200

    [NEW] ej 10 constante

--- src/ejerciciostema1/Ejercicio10.java
+++ src/ejerciciostema1/Ejercicio10.java
@@ -6,13 +6,14 @@ public class Ejercicio10 {
 
 	public static void main(String[] args) {
 		// Variables
-		float precio, iva = 21, total;
+		float precio;
+		final float IVA = 21, total;
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Cual es el precio del producto? ");
 		precio = sc.nextFloat();
 		
-		total = precio + ((precio * iva)/100f);
+		total = precio + ((precio * IVA)/100f);
 		
 		System.out.println("El precio total es de " + total + " euros");
 		sc.close();
