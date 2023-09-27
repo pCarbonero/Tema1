68c0f5ba40646e3f2f320e165794b05dc8706f5c
pcarbonero <pcarbonero@106-19.info2>
2023-09-27 09:43:09 +0200

    [NEW] ej 2

--- src/ejerciciosentregatema1/Ejercicio2.java
+++ src/ejerciciosentregatema1/Ejercicio2.java
@@ -1,9 +1,33 @@
 package ejerciciosentregatema1;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Variable para  almacenar el numero entero
+		int num;
+		// int para almacenar cuanto le falta al numero para ser multiplo de 7
+		int res;
+		//constante para dividir num entre 7
+		final int CONST = 7;
+		//variable escaner para recoger info del teclado
+		Scanner sc = new Scanner(System.in);
+		
+		// pedimo el numero entero 
+		System.out.println("dame un numero entero ");
+		//almaceno el numero insertado en la variable num
+		num = sc.nextInt();
+		
+		//averiguo el resto del numero int entre 7
+		res = CONST - ( num % CONST);
+		
+		//muestro en pantalla el numero 
+		System.out.println("Para que sea múltiplo de 7 necesitas sumarle " + res);
+		
+		//cierro el escaner 
+		sc.close();
+		
 
 	}
 
