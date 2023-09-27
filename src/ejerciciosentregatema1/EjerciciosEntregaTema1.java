c22d01a49190d06456ffdf19e7440909bcff7a6e
pcarbonero <pcarbonero@106-19.info2>
2023-09-27 09:18:05 +0200

    [NEW] ejercicios entrega tema 1

---
+++ src/ejerciciosentregatema1/EjerciciosEntregaTema1.java
@@ -0,0 +1,41 @@
+package ejerciciosentregatema1;
+
+import java.util.Scanner;
+
+public class EjerciciosEntregaTema1 {
+
+	public static void main(String[] args) {
+		//Variable float para almacenar el numero con decimales
+		float num;
+		//variable float para guardar solo la parte decimal del 
+		//numero introducido
+		float decimal;
+		//variable para sacar la comparacion  de la parte decimal con 0.5
+		float comparacion;
+		//variable para guardar el valor redondeado
+		int resultado;
+		
+		//variable Scanner para crear el escaner que recoge info del teclado
+		Scanner sc = new Scanner(System.in);
+		
+		//pregunto al usuario por el numero para que lo introduzca
+		System.out.println("Escribe un número ");
+		
+		//almaceno el numero que has introducido en la variable num
+		num = sc.nextFloat();
+		//calculo cual es la parte decimal del numero introducido para tenrlo por separado
+		decimal = num - (int) num;
+		
+		//comparo la variable decimal con 0.5 para saber hacia donde hay que redondearlo
+		//y lo almaceno en la variable resultado
+		comparacion = (decimal >= 0.5f) ? resultado = ((int) num + 1) : (resultado = (int) num);
+		
+		
+		//devuelvo por consola el resultado final
+		System.out.println("Tu resultado es " + resultado);
+		
+		//cierro el escaner
+		sc.close();
+	}
+
+}
