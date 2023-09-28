5dcf704c0437cf9d6573f49b70d24536d7e391b9
pcarbonero <pcarbonero@106-19.info2>
2023-09-28 12:27:57 +0200

    [NEW] ej 5

---
+++ src/ejerciciosentregatema1/Ejercicio5.java
@@ -0,0 +1,43 @@
+package ejerciciosentregatema1;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		// creamos las variables de los valores a, b, c y x
+		float a, b, c, x;
+		//variable para almacenar el resultado
+		float y;
+		//varaible para el esaner que recpge info del teclado
+		Scanner sc = new Scanner(System.in);
+		
+		//pedimos los distintos valores
+		System.out.println("Escribe un número");
+		//almacenamos el valor introducid en la variable
+		a = sc.nextFloat();
+		
+		//pedimos los distintos valores
+		System.out.println("Escribe un número");
+		//almacenamos el valor introducid en la variable
+		b = sc.nextFloat();
+		
+		//pedimos los distintos valores
+		System.out.println("Escribe un número");
+		//almacenamos el valor introducid en la variable
+		c = sc.nextFloat();
+		
+		//pedimos los distintos valores
+		System.out.println("Escribe un número");
+		//almacenamos el valor introducid en la variable
+		x = sc.nextFloat();
+		
+		//escribimos el calculo
+		y = (float) Math.pow(x, 2f) + b*x + a;
+		
+		//devolvemos el valor por consola
+		System.out.println("El valor de y es de " + y);
+
+	}
+
+}
